7219b589a97fe28840f7a2c61affca992e65ecd4
Steve Zelek <szelek@chathamfinancial.com>
2017-11-19 01:18:46 -0500

    Finished default page

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-//
 import './styles.scss';
+
+var showNext = function(i) {
+    document.getElementById('next' + i).classList.add('hidden');
+    document.getElementById('part' + (i + 1)).classList.remove('hidden');
+}
+
+document.getElementById('next1').addEventListener ("click", () => showNext(1), false);
+document.getElementById('next2').addEventListener ("click", () => showNext(2), false);
+document.getElementById('next3').addEventListener ("click", () => showNext(3), false);
