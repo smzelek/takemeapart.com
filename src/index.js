8e55cc282d9bb3cf1eb7600ba217ccdcb1cdf4d0
Steve Zelek <szelek@chathamfinancial.com>
2017-11-22 01:18:11 -0500

    Site pre-editing completed.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,46 @@
 import './styles.scss';
 
-var showNext = function(i) {
-    document.getElementById('next' + i).classList.add('hidden');
-    document.getElementById('part' + (i + 1)).classList.remove('hidden');
+var devName = "";
+var serverOn = true;
+
+var showPanel2 = function () {
+    devName = document.getElementsByTagName('input')[0].value;
+    if (devName == "") return;
+    document.getElementById('next1').classList.add('hidden');
+    document.getElementById('part2').classList.remove('hidden');
+    document.getElementsByTagName('input')[0].setAttribute('disabled', 'true')
+    document.getElementById('developer').classList.remove('invisible');
+    document.getElementById('owner').innerText = "property of " + devName;
 }
 
-document.getElementById('next1').addEventListener ("click", () => showNext(1), false);
-document.getElementById('next2').addEventListener ("click", () => showNext(2), false);
-document.getElementById('next3').addEventListener ("click", () => showNext(3), false);
+var showPanel3 = function (i) {
+    document.getElementById('next2').classList.add('hidden');
+    document.getElementById('part3').classList.remove('hidden');
+    document.getElementById('server').classList.remove('invisible');
+}
+
+var showPanel4 = function (i) {
+    document.getElementById('next3').classList.add('hidden');
+    document.getElementById('part4').classList.remove('hidden');
+}
+
+var toggleServer = function () {
+    if (serverOn) {
+        document.getElementById('content').classList.add('hidden');
+        document.getElementById('offlineContent').classList.remove('hidden');
+        document.getElementById('title').classList.add('offline');
+        document.getElementById('serverPower').classList.add('offline');
+    }
+    else {
+        document.getElementById('offlineContent').classList.add('hidden');
+        document.getElementById('content').classList.remove('hidden');
+        document.getElementById('title').classList.remove('offline');
+        document.getElementById('serverPower').classList.remove('offline');
+    }
+    serverOn = !serverOn;
+}
+
+document.getElementById('next1').addEventListener("click", () => showPanel2(), false);
+document.getElementById('next2').addEventListener("click", () => showPanel3(), false);
+document.getElementById('next3').addEventListener("click", () => showPanel4(), false);
+document.getElementById('serverPower').addEventListener("click", () => toggleServer(), false);
